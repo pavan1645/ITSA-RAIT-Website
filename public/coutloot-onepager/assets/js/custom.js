b5b9ea77fe1ec349f7cff0e0949c6f4017c40fcb
pavan1645 <pavan1645@gmail.com>
2018-03-08 18:37:33 +0530

    Added forms and navbar changed

--- public/coutloot-onepager/assets/js/custom.js
+++ public/coutloot-onepager/assets/js/custom.js
@@ -1,32 +1,28 @@
 var content = [
 	{
-		"title": "News",
-		"desc": "<a><h5 id='one1'> — February 06, 2018</h5><h5> A Q&A with Antony Jenkins</h5></a><h6>10x Future Technologies Founder & Executive Chairman Antony Jenkins discusses the challenges and opportunities facing banks, leadership, politics and his love of Miles Davis. Click here to listen to his podcast Q&A with Westpac Wire.</h6><br><a onclick='displaySpan(this)'><h5> — January 26, 2018?</h5><h5 id='one2'>Open banking – what is it and why does it matter</h5></a><br style='display:none'><h6>Open banking is now a reality. So what’s changing and what will the impact be for businesses and consumers?<span style='display:none'>Open banking is now a reality. Earlier this month, regulations came into force that will make it much easier for customers of UK banks to access better, cheaper and more personalised products from other providers. <br><br>In 2016, a report from the government’s competition authority found that large banks weren’t having to compete very hard for customers, and that new providers were effectively being shut out of the market as a result. Because banks haven’t had to treat you well to keep you as a customer, the consequence has been expensive products and poor services. Those who have experienced punitive charges for overdrafts, or who were mis- sold PPI, are well aware of what it is like to be poorly served by the banks. <br><br>So what’s changing? <br><br></span></h6><br><a><h5 id='one3'> — January 23, 2018</h5><h5>Making banking 10x better</h5></a><h6>10x Future Technologies Founder and Executive Chairman Antony Jenkins talks to Will Beeson, Creator and Host of Rebank – Banking the Future, in this podcast that looks at how 10x is transforming financial services through technology to make banking better for banks, their customers and society.Click here to listen to the podcast.</h6><br><a><h5> — January 09, 2018</h5><h5>The future of banking</h5></a><h6>Want to know more about how 10x Banking is helping banks and other businesses transform with customer- focused technology? Click here to see 10x’s Antony Jenkins discuss the future of banking and more with the Financial Times’ Banking Editor, Martin Arnold.FT on Air on Facebook.</h6><br>",
+		"title": "Techie",
+		"desc": $('.sidenav-contents .techie').html(),
 		"color": "#63c0c6"
 	},
 	{
-		"title": "About",
-		"desc": "<h5> About 10x</h5> <h6>We are a team of financial services experts, technologists and creatives designing the future of money. <br> We exist to create a new banking experience — one that is more diverse, open and fair. <br>We challenge industry norms by empowering our partners to build products with clear benefit to people and society. <br>We are building a universal platform built on open banking principles, infinitely configurable and constantly evolving to deliver better, more personal, experiences for everyone.</h6>",
+		"title": "FAQs",
+		"desc": $('.sidenav-contents .faq').html(),
 		"color": "#c0d2e7"
 	},
 	{
-		"title": "People",
-		"desc": "<br><h5>Antony Jenkins. Founder & Executive Chairman </h5><h6> As Founder and Executive Chairman of 10x Future Technologies, Antony leads the company that promises to redefine how banks operate and engage with customers.</h6><br><h5>Rebecca Skitt. Chief Operating Officer </h5><h6> As Chief Operating Officer, Rebecca is leading the development of 10x’s culture and capabilities to re-imagine banking to make peoples lives much easier.</h6><br><h5>Mark Allcock. Chief Innovation Officer </h5><h6> As Chief Innovation Officer, Mark is leading the design of the 10x platform to transform financial services.</h6><br><h5>Tam Holmes. Chief Digital & Trust Officer </h5><h6> As Chief Digital & Trust Officer, Tam is leading the delivery of our next generation digital banking capabilities and experiences to our global clients.</h6><br><h5>Sallyann Brookes. Chief Human Resources Officer </h5><h6> As Chief HR Officer, Sallyann leads the people agenda to allow great people to thrive in 10x and ensure its a 10x better place to work.</h6><br><h5>Richard Given. General Counsel </h5><h6> As General Counsel, Richard provides legal advice to support the 10x team to deliver transformational services in line with our core value of integrity.</h6><br><h5>Amanda Jenkins. Chief Impact Officer </h5><h6> As Chief Impact Officer, Amanda leads the 10x Impact Programme bringing to life one of our core values.</h6>",
+		"title": "What do we do?",
+		"desc": $('.sidenav-contents .wdwd').html(),
 		"color": "#bec0e0"
 	},
 	{
-		"title": "Impact",
-		"desc": "<a onclick='displaySpan(this)'><h5 id='two1'>Anti Modern Slavery Statement</h5></a><br><h6>This statement sets out 10x’s actions to understand all potential modern slavery risks related to its business and to put in place steps that are aimed at ensuring that there is no slavery or human trafficking in its own business and its supply chains.This statement relates to actions and activities during the financial year ending December 2017 <br><br><span style='display:none'>In keeping with our values of Integrity and Impact, at 10x we care about the world we live in and strive to make it better and we seek opportunities through our work to have a positive impact particularly for those less advantaged. As part of this we recognises that we have a responsibility to take a robust approach to slavery and human trafficking.<br><br>10x is absolutely committed to preventing slavery and human trafficking in our activities, and to ensuring that our partners and our supply chains are free from slavery and human trafficking. We require vendors and suppliers to maintain and promote fundamental human rights, where employment decisions are based on free choice without any coerced or prison labour, no use of physical punishment or threats of violence or other forms of physical, sexual, psychological or verbal abuse as a method of discipline or control.</span></h6><br><a onclick='displaySpan(this)'><h5 id='two2'>Diversity & Inclusion</h5></a><br><h6>At 10x, we are committed to promoting diversity and creating an inclusive culture.<br><br><span style='display:none'>Our business is about people. We live and operate in a diverse world and at 10x we are embracing the opportunity to ensure our organisation reflects this, both in people and thought. We strive to create an environment responsive to different cultures, groups and in all our interactions with our people, partners, customers, visitors, suppliers, contractors, investors and in the communities in which we operate.<br><br>We value diversity. Fundamentally we believe it is the right thing to do. We also believe it to be critical to our effectiveness, our business success and integral to achieving our strategic objective of being 10x better and 10x better company to work for.</span></h6><br><a onclick='displaySpan(this)'><h5 id='two3'>Women in Finance Charter</h5></a><br><h6>10x has signed up to The Women in Finance Charter.<br><br><span style='display:none'>At 10x, we are committed to promoting diversity and creating an inclusive culture in an organisation that reflects the diverse world in which we live and <ul id=\"menu-sidenav\"><li onclick=\"loadSideNav(0, 'one1')\">News</li><li onclick=\"loadSideNav(1, '')\">About</li><li onclick=\"loadSideNav(2, '')\">People</li><li onclick=\"loadSideNav(3,'two1')\">Impact</li><li onclick=\"loadSideNav(4,'two1')\">Jobs</li></ul>operate.<br><br>We value diversity. Fundamentally we believe it is the right thing to do. We also believe it to be critical to our effectiveness, our business success and integral to achieving our strategic objective of being 10x better and 10x better company to work for.</span></h6>",
-		"color": "#fbcc40"
+		"title": "Apply",
+		"desc": $('.sidenav-contents .apply').html(),
+		"color": "#fff"
 	},
-	{
-		"title": "Jobs",
-		"desc": "<h5>Work with us</h5><h6>Do you want to be part of our transformation journey?<br><br>At 10x we know the future is going to be radically different from the past. So we are designing the future of money, reimagining the customer experience and obsessing about how to improve people’s daily lives and make things 10x better.<br><br>We are building a universal platform built on open banking principles, infinitely configurable and constantly evolving to deliver better, more personal, experiences for everyone.<br><br>We exist to transform the industry and be a catalyst for positive change. Guided by strong values of Integrity, Transformation and Impact, we actively support, respect and collaborate with our customers, partners and with each other.<br><br>We are a unique team of financial services experts, technologists and creatives, united by a passion for change, approaching challenges in a radically different way and never ever compromising on achieving a 10x better experience for our customers, for society and for the 10x Team.<br><br>careers@10xbanking.com</h6>",
-		"color": "#bec0e0"
-	},
+	//Add here new
 	{
 		"title": "Menu",
-		"desc": "<ul id=\"menu-sidenav\"><li onclick=\"loadSideNav(0, 'one1')\">News</li><li onclick=\"loadSideNav(1, '')\">About</li><li onclick=\"loadSideNav(2, '')\">People</li><li onclick=\"loadSideNav(3,'two1')\">Impact</li><li onclick=\"loadSideNav(4,'two1')\">Jobs</li></ul>",
+		"desc": "<ul id=\"menu-sidenav\"><li><a onclick=\"closenav()\">Techie</a></li><li><a onclick=\"loadSideNav(1, '')\">FAQs</a></li><li><a onclick=\"loadSideNav(2, '')\">What do we do?</a></li><li><a onclick=\"loadSideNav(3, '')\">Apply</a></li></ul>",
 		"color": "#22222a"
 	}
 ]
@@ -41,15 +37,14 @@ $(window).on("load", function () {
 });
 
 $(document).ready(function () {
+	$('select').formSelect();
 	$('.sidenav').sidenav({ "edge": "right" });
 	window.addEventListener("resize", function () {
 		resize();
 	});
-
 	$(document).mouseup(function (e) {
 		var container = $("#custom-sidenav");
 		var snTrigger = $('.sidenav-trigger');
-
 		// if the target of the click isn't the container nor a descendant of the container
 		if (!container.is(e.target) && container.has(e.target).length === 0 && isSidenavOpen) {
 			isSidenavOpen = false;
@@ -74,12 +69,28 @@ function loadSideNav(index, dest) {
 		isSidenavOpen = true;
 		$('#custom-sidenav, #custom-sidenav .header').css("background-color", content[index].color);
 		/* If its menu sidenav change colors to white */
-		if (index == 5) {
+		if (index == content.length-1) {
 			$('#custom-sidenav .header').css("color","white");
 			isSidenavOpen = false;
 		} else $('#custom-sidenav .header').css("color", "rgb(36, 36, 36)");
+		
 		$('#custom-sidenav .sidenav-title').text(content[index].title);
 		$('#custom-sidenav .desc').html(content[index].desc);
+		
+		/* Code for forms */
+		if (content[index].title == "Apply") {
+			$('select').formSelect();
+			$('select').change(function () {
+				if ($(this).val() != "0") $('.expDesc').css('display', 'block')
+				else $('.expDesc').css('display', 'none')
+			});
+			
+			$(".form").submit(function (e) {
+				e.preventDefault();
+				formSubmit(e);
+			});
+		}
+		
 		$('#custom-sidenav').css("width",sidenavWidth);
 		$(".hover").css("pointer-events", "none");
 	} else {
@@ -94,13 +105,6 @@ function closenav() {
 	$(".hover").css("pointer-events", "auto");
 }
 
-function closeDefaultNav() {
-	//close mobile default sidenav
-	var elem = document.querySelector('.sidenav');
-	var instance = M.Sidenav.init(elem);
-	instance.close();
-}
-
 function openUrl(url) {
 	window.open("https://"+url, "_blank")
 }
@@ -108,4 +112,20 @@ function openUrl(url) {
 function displaySpan(el) {
 	if ($(el).next().next().find("span").css("display") == 'none') $(el).next().next().find("span").css("display", "block");
 	else $(el).next().next().find("span").css("display", "none")
+}
+
+function formSubmit(e) {
+	var fd = new FormData();
+	fd.append('inputs',$("#form").serializeArray());
+	var myFile = $('#file')[0].files[0];
+	fd.append('file',myFile);
+	console.log(fd.get("inputs"));
+	console.log(fd.get("file"));
+}
+
+//Scrolls to top of the page
+function st() {
+	$('html, body').animate({
+		scrollTop: 0
+	}, 1000);
 }
